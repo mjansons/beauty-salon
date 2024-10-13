437fa083a41c204c6cb82fb9163ded1108a98beb
Matiss Jansons <matiss93jansons@gmail.com>
2024-10-13 20:19:50 +0300

    fix: typecheck

---
+++ client/src/components/FooterElement.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer>
+    <div class="footer-top-content">
+      <RouterLink :to="{ name: 'home' }" tabindex="-1">
+      <img class="logo" alt="wink logo" src="../assets/images/wink.svg" />
+    </RouterLink>
+    <div class="footer-links">
+      <p>Link One</p>
+      <p>Link Two</p>
+      <p>Link Three</p>
+      <p>Link Four</p>
+      <p>Link Five</p>
+    </div>
+    </div>
+
+    <div class="footer-bottom-content">
+      <p>2024 Wink. All right reserved.</p>
+      <div class="bottom-right-content">
+        <p>Privacy Policy</p>
+        <p>Terms of Service</p>
+        <p>Cookies Settings</p>
+      </div>
+    </div>
+
+  </footer>
+</template>
+
+<style scoped>
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--white);
+    padding: 64px;
+  }
+
+  .footer-top-content{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 32px;
+  }
+
+  .footer-links{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 32px;
+    padding-bottom: 32px;
+  }
+
+  .footer-bottom-content {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 100%;
+    border-top: 1px solid var(--purple-900);
+    padding-top: 32px;
+
+    & p {
+      font-family: Roboto, sans-serif;
+      font-weight: 300;
+      font-size: var(--small);
+      text-align: center;
+      margin-bottom: 24px;
+    }
+  }
+
+  .bottom-right-content{
+    display: flex;
+    gap: 24px;
+    flex-direction: row;
+    justify-content: center;
+    text-decoration: underline;
+  }
+</style>
---
+++ client/src/components/HowItWorks.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="main-wrapper">
+    <h1>How it works</h1>
+    <div class="info-box-wrapper">
+      <div class="info-box">
+        <img src="../assets/images/search.svg" alt="search icon" />
+        <div class="box-texts">
+          <h3>Select Your Service & Time</h3>
+          <p>
+            Choose the beauty or wellness service you need and the time that
+            works best for you—whether it’s today, next month, or within a
+            specific time frame.
+          </p>
+        </div>
+      </div>
+      <div class="info-box">
+        <img src="../assets/images/personalcard.svg" alt="search icon" />
+        <div class="box-texts">
+          <h3>View Available Salons & Prices</h3>
+          <p>
+            Instantly see a list of salons that match your selected times,
+            complete with pricing for each service.
+          </p>
+        </div>
+      </div>
+      <div class="info-box">
+        <img src="../assets/images/calendar-tick.svg" alt="search icon" />
+        <div class="box-texts">
+          <h3>Book with Ease</h3>
+          <p>
+            Pick your preferred salon and time, and confirm your booking with a
+            tap. You're all set!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-wrapper {
+  background-color: var(--white);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 64px;
+  padding: 64px;
+}
+
+h1 {
+  font-family: Calistoga, sans-serif;
+  font-size: var(--extra-extra-large);
+  text-align: center;
+}
+
+.info-box-wrapper {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 48px;
+}
+
+.info-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+  max-width: 300px;
+}
+
+.box-texts {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+h1 {
+  margin: 0px;
+}
+
+h3 {
+  font-family: Calistoga, sans-serif;
+  font-size: var(--large);
+  text-align: center;
+  margin: 0;
+}
+
+p {
+  font-size: var(--small);
+  text-align: center;
+  font-weight: 300;
+}
+</style>
